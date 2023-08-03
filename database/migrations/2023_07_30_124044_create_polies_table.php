d0210937b144cedcec00c491dee5681f1ad27f93
musfira13 <aakunku18@gmail.com>
2023-08-03 08:38:38 +0800

    edit data poly

--- database/migrations/2023_07_30_124044_create_polies_table.php
+++ database/migrations/2023_07_30_124044_create_polies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('polies', function (Blueprint $table) {
+        Schema::create('poly', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('ruangan');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('polies');
+        Schema::dropIfExists('poly');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BackOffice\PolyController;
+use App\Interfaces\PolyInterfaces;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('v1/poly')->controller(PolyController::class)->group(function() {
+  Route::get    ('/'     , 'getAllData'  );
+  Route::get    ('/{id}' , 'getDataById' );
+  Route::post   ('/'     , 'upsertData'  );
+  Route::delete ('/{id}' , 'deleteData'  );
 });
