f963bf9743a17da5c415a4aff063e230e8f1e20a
musfira13 <aakunku18@gmail.com>
2023-08-10 22:00:17 +0800

    tambah tabel profile dokter

--- app/Models/doctor_profile.php
+++ app/Models/doctor_profile.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class doctor_profile extends Model
 {
     use HasFactory;
-    protected $table = 'doctor_profile';
+    protected $table = 'doctorprofile';
     protected $fillable = [
         'user_id',
         'nama',
@@ -21,4 +21,26 @@ class doctor_profile extends Model
         'tgl_lahir',
         'agama',
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopejoinList($query)
+    {
+        return $query ->leftJoin('user as model_a', 'doctorprofile.user_id', '=', 'model_a.id')
+        ->select(
+            'doctorprofile.id', 
+            'model_a.username',
+            'doctorprofile.nama',
+            'doctorprofile.alamat',
+            'doctorprofile.no_hp',
+            'doctorprofile.jk',
+            'doctorprofile.email',
+            'doctorprofile.pekerjaan',
+            'doctorprofile.status',
+            'doctorprofile.tgl_lahir',
+            'doctorprofile.agama',
+        );   
+    }
 }
---
+++ app/Repositories/DoctorProfileRepository.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\DoctorProfileInterfaces;
+use App\Models\doctor_profile;
+use App\Models\profile;
+
+class DoctorProfileRepository implements DoctorProfileInterfaces
+{ 
+    private doctor_profile $doctorprofileModel;
+    public function __construct(doctor_profile $doctorprofileModel)
+    {
+      $this->doctorprofileModel = $doctorprofileModel;
+    }
+  
+    public function getAllPayload(array $params)
+    {
+      try {
+        $payloadList = $this->doctorprofileModel->all();
+        $responseJson = array(
+          'code'    => 200,
+          'message' => 'success get data',
+          'data'    => $payloadList,
+          'meta'    => [
+            'total' => $payloadList->count()
+          ]
+        );
+      } catch (\Exception $th) {
+        $responseJson = array(
+          'code'    => 500,
+          'message' => $th->getMessage(),
+          'meta'    => [
+            'function' => 'DoctorProfileRepository - getAllPayload'
+          ]
+        );
+      }
+      return $responseJson;
+    }
+    
+    public function getPayloadById(int $idPayload)
+    {
+      try {
+  
+        $whereData = $this->doctorprofileModel->whereId($idPayload)->first();
+  
+        if ($whereData) {
+          $responseJson = array(
+            'code'    => 200,
+            'message' => 'success get data',
+            'data'    => $whereData
+          );
+        } else {
+          $responseJson = array(
+            'code'    => 404,
+            'message' => 'not found'
+          );
+        }
+  
+      } catch (\Throwable $th) {
+        $responseJson = array(
+          'code'    => $th->getCode(),
+          'message' => $th->getMessage(),
+          'meta'    => [
+            'function' => 'DoctorProfileRepository - getPayloadById'
+          ]
+        );
+      }
+  
+      return $responseJson;
+    }
+  
+    public function upsertPayload($idPayload, array $payload)
+    {
+        try {
+          if ($idPayload) {
+            $findData = $this->getPayloadById($idPayload);
+    
+            if ($findData['code'] == 404) {
+              return $findData;
+            }
+    
+            $whereData = $this->doctorprofileModel->whereId($idPayload);
+            $responseJson = array(
+              'code'    => 200,
+              'message' => 'success update data',
+              'data'    => $whereData->update($payload)
+            );
+          } else {
+            $responseJson = array(
+              'code'    => 200,
+              'message' => 'success create data',
+              'data'    => $this->doctorprofileModel->create($payload)
+            );
+          }
+          
+        } catch (\Throwable $th) {
+          $responseJson = array(
+            'code'    => $th->getCode(),
+            'message' => $th->getMessage(),
+            'meta'    => [
+              'function' => 'DoctorProfileRepository - upsertPayload'
+            ]
+          );
+        }
+    
+        return $responseJson;
+    }
+  
+    public function deletePayload(int $idPayload)
+    {
+      try {
+        $findData = $this->getPayloadById($idPayload);
+        if ($findData['code'] == 404) {
+          return $findData;
+        }
+  
+        $whereData = $this->doctorprofileModel->whereId($idPayload);
+        $responseJson = array(
+          'code'    => 200,
+          'message' => 'success delete data',
+          'data'    => $whereData->delete()
+        );
+      } catch (\Throwable $th) {
+        $responseJson = array(
+          'code'    => $th->getCode(),
+          'message' => $th->getMessage(),
+          'meta'    => [
+            'function' => 'DoctorProfileRepository - deletePayload'
+          ]
+        );
+      }
+  
+      return $responseJson;
+    }
+}
